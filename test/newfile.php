93b7254f643f17e4d23b02052ebc971fe30c7a13
junxun_cl <chenlang@qq.com>
2015-08-15 17:32:41 +0800

    一个测试提交

---
+++ test/newfile.php
@@ -0,0 +1,518 @@
+
+<htlm>
+<head><style type="text/css"></style></head>
+<body><htlm>
+<style type="text/css"></style>
+
+<div style="height:400px;position:relative">
+
+ <img style=" margin-left: 480px;margin-bottom: 20px;position:absolute;z-index:1;width: 320px;margin-top: 12px;height: 340px;" src="muwen.png"> 
+   <div style="margin-left: 500px;position:absolute;z-index:2;width: 280px;margin-top: 35px;height: 298px;">
+<img style="
+    margin-top: -13px;
+    border-radius: 20px;
+    margin-right: -13px;
+" width="30" height="30" align="right" id="a7" onclick="ona7()" src="black.png">
+  <img style="
+
+    margin-top: 112px;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right" width="30" height="30" id="d7" onclick="ond7()" src="wihter.png">
+  <img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 70px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right" id="c7" onclick="onc7()">
+  <img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right" id="b7" onclick="onb7()">
+  
+  <img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 154px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right"><img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 197px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+  <img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 279px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+  <img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 238px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+</div>
+  <table style="
+    margin-left: 500px;position:absolute; z-index:1;margin-top:35px
+" border="1">
+  
+<tbody>
+  <tr style="height: 40px;">
+  <td style="width: 40px"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="a1" onclick="ona1()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="a2" onclick="ona2()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="a3" onclick="ona3()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="a4" onclick="ona4()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="a5" onclick="ona5()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="a6" onclick="ona6()"></td>
+
+
+  </tr>
+ 
+   <tr style="height: 40px;">
+  <td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="b1" onclick="onb1()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="b2" onclick="onb2()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="b3" onclick="onb3()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="b4" onclick="onb4()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="b5" onclick="onb5()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="b6" onclick="onb6()"></td>
+
+
+  </tr>
+   <tr style="height: 40px;">
+  <td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="c1" onclick="onc1()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="c2" onclick="onc2()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="c3" onclick="onc3()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="c4" onclick="onc4()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="c5" onclick="onc5()"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+" id="c6" onclick="onc6()"></td>
+
+
+  </tr>
+   <tr style="height: 40px;">
+  <td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+
+
+  </tr>
+   <tr style="height: 40px;">
+  <td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+
+
+  </tr>
+   <tr style="height: 40px;">
+  <td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"></td>
+
+
+  </tr>
+   <tr style="height: 40px;">
+  <td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    margin-bottom: -40px;
+    border-radius: 20px;
+    margin-left: -30px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    margin-bottom: -40px;
+    border-radius: 20px;
+    margin-left: -30px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    margin-bottom: -40px;
+    border-radius: 20px;
+    margin-left: -30px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    margin-bottom: -40px;
+    border-radius: 20px;
+    margin-left: -30px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    margin-bottom: -40px;
+    border-radius: 20px;
+    margin-left: -30px;
+"></td>
+<td style="width: 40px;"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-top: -35px;
+    margin-left: -20px;
+"><img style="
+    width: 30px;
+    height: 30px;
+    background-color: black;
+    margin-bottom: -40px;
+    border-radius: 20px;
+    margin-left: -30px;
+"></td>
+
+
+  </tr>
+  </tbody></table>
+  
+</div>
+<script>
+// 1 代表黑子
+// 2 代表白子
+  var one = 1;
+  var a7=document.getElementById("a7");
+  var d7=document.getElementById("d7");
+  
+function ona7()
+{
+	if(one == 1){
+		a7.src = "black.png";
+		one = 2 ;
+		}
+	else{
+		a7.src = "wihter.png";
+		one = 1 ;
+		}
+ }
+function ond7()
+{
+	if(one == 1){
+		d7.src = "black.png";
+		one = 2 ;
+		}
+	else{
+		d7.src = "wihter.png";
+		one = 1 ;
+		}
+ }
+</script>
+
+</htlm></body>
+</htlm>
---
+++ test/newfile1.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+
+<div style="height: 800px;position:relative">
+
+<div style="margin-left: 500px;position:absolute;z-index:2;width: 280px;margin-top: 12px;height: 298px;">
+<img width="30" height="30"   align="right" id = "video1" onclick="makeNormal()">
+<img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 112px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+<img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 70px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+<img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 30px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+
+<img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 154px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right"><img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 197px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+<img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 279px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+<img style="
+    width: 30px;
+    height: 30px;
+    margin-top: 238px;
+    background-color: black;
+    border-radius: 20px;
+    margin-right: -33px;
+" align="right">
+</div>
+
+
+<script type="text/javascript">
+var myVideo=document.getElementById("video1");
+
+function makeNormal()
+{ 
+myVideo.width=50; 
+} 
+</script> 
+
+</body> 
+</html>
